af7389ce622b658329138c34910d048d4eb6ca61
AlbertYuan <yuanliangios@163.com>
2021-10-11 18:07:17 +0800

    Initial Commit

---
+++ YLInfiniteScrollView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YLInfiniteScrollView
+//
+//  Created by AlbertYuan on 2021/10/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
